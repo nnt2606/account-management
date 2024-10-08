5c40f725fbf9f13c66357a70c432b064c1400f48
Ngoc Tu <ngoctu2662@gmail.com>
2024-10-08 12:08:29 +0700

    update 08/10/24 12:08PM
    save session store

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as session from 'express-session';
-import * as passpost from 'passport';
+import session from 'express-session';
+import passpost from 'passport';
+import MongoStore from 'connect-mongo';
 
 
 async function bootstrap() {
@@ -16,7 +17,12 @@ async function bootstrap() {
       cookie: {
         maxAge: 60*60*1000,
         secure: false
-      }
+      },
+      store: MongoStore.create({
+        mongoUrl: 'mongodb://localhost/account-management',
+        collectionName: 'sessions',
+        autoRemove: 'native',
+      })
     })
   );
 
